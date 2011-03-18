4274048a030477ecb40e6fa1f7580dc20756a92f
jm2dev <jomarcar@gmail.com>
2011-03-18 11:48:49 +0000

    xrandr script to setup my two displays.

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ Overview
 My configuration files to work in an efficient way, or I think so :)
 
 emacs
-==========
+=====
 
 My customizations to show line numbers, and use several modes for grails, groovy and XML development.
+
+xrandr
+======
+
+Script to configure my dual display desktop.
 
---
+++ xrandr/multimonitor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+xrandr --output DVI-0 --mode 1440x900 --output DVI-1 --mode 1440x900 --right-of DVI-0
+#xrandr --output DFP1 --mode 1440x900 --output DFP2 --mode 1440x900 --right-of DFP1
+#xrandr --output DVI-I_1/digital --mode 1440x900 --output DVI-I_2/digital --mode 1440x900 --right-of DVI-I_1/digital
+#xrandr --output DVI-1 --left-of DVI-0
+#xrandr --output DVI-I_1/digital --mode 1440x900 --rate 75 --output DVI-I_2/digital --mode 1440x900 --rate 75 --right-of DVI-I_1/digital
+#xrandr --output DVI-0 --right-of DVI-1
+#xrandr --output DFP2 --right-of DFP1
